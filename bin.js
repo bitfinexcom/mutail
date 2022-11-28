32dbe2b03d2e3b0c73f4544d890d8e311ba1ded1
prdn <paolo.ardoino@gmail.com>
2022-11-28 14:41:31 -0500

    initial commit

---
+++ bin.js
@@ -0,0 +1,28 @@
+const argv = require('minimist')(process.argv.slice(2))
+const FilesWatcher = require('./lib/watcher')
+
+const helpMsg = 'Usage:\nmutail -f file(s)'
+
+if (argv.help) {
+  console.log(helpMsg)
+  process.exit(-1)
+}
+
+if (!argv.f) {
+  console.error('Error: file/pattern invalid')
+  process.exit(-1)
+}
+
+const watcher = new FilesWatcher(argv.f, 'utf-8', {
+  useFilePrefix: true
+})
+
+watcher.on('data', data => {
+  console.log(data)
+})
+
+process.once('SIGINT', () => {
+  watcher.stop()
+})
+
+watcher.start()
